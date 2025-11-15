692204b1b239c8be27a060ccc240258661c41e32
Subhankar Garai <subhankargarai954@gmail.com>
2025-11-15 21:21:43 +0530

    updated pool and gitignore file

--- config/pool.js
+++ config/pool.js
@@ -1,19 +1,28 @@
 // pool.js
-
 import pkg from "pg";
 const { Pool } = pkg;
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
-});
+const useDatabaseUrl = !!process.env.DATABASE_URL;
+const enableSsl = process.env.DB_SSL === "true" || false;
+
+const poolConfig = useDatabaseUrl
+    ? {
+          connectionString: process.env.DATABASE_URL,
+          ssl: enableSsl ? { rejectUnauthorized: false } : false,
+      }
+    : {
+          user: process.env.DB_USER,
+          host: process.env.DB_HOST,
+          database: process.env.DB_NAME,
+          password: process.env.DB_PASSWORD,
+          port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
+          ssl: enableSsl ? { rejectUnauthorized: false } : false,
+      };
+
+const pool = new Pool(poolConfig);
 
 pool.on("connect", () => {
     console.log(`Connected to the database`);
